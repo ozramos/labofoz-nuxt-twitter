5d1b78cf7577f5cc7d6dae4884a6634b7b11cb6b
Oz Ramos <labofoz@gmail.com>
2019-07-06 15:23:21 -0700

    #1 Initial commit with pug and stylus

---
+++ components/Logo.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  img.VueToNuxtLogo(src='/favicon.png')
+</template>
+
+<style lang="stylus">
+.VueToNuxtLogo
+  display inline-block
+  animation turn 1s ease
+  transform rotateY(0deg)
+  position relative
+  overflow hidden
+  height 240px
+  width 240px
+
+@keyframes turn
+  0%
+    width 0
+    height 0
+
+  100%
+    transform rotateY(1440deg)
+    width 240px
+    height 240px
+</style>
