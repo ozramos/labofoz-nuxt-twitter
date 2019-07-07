6e13c500468f44dcf8963bfec715f959312b0b3d
Oz Ramos <labofoz@gmail.com>
2019-07-06 17:37:49 -0700

    #2 Adds tilt.js to logo

--- pages/index.vue
+++ pages/index.vue
@@ -5,62 +5,88 @@
         small
           small the
         |  Lab of Oz
-      p Hey 👋 I'm Oz and I love the future, <strong>computer vision</strong>, and <strong>machine learning</strong>!
-      br
-      logo
+      p Hey 👋 I'm Oz and I love the future,<br><strong>computer vision</strong>, and <strong>machine learning</strong>!
 
     ul
-      li
+      li(v-for='link in links')
         a
-          | Handsfree.js
-          br
-          span Drop-in library for crafting handsfree interfaces
-      li
-        a 
-          | School of AI
-          br
-          span Awarded Fellowship in 2018
-      li
-        a
-          | Nonprofiters
-          br
-          span Resource guide for public services and assistance programs
-      li
-        a
-          | STUDIO Residency
-          br
-          span Residency at the Studio for Creative Inquiry at Carnegie Mellon
-      li
-        a LinkShow
-      li
-        a More...
+          | {{link.title}}
+          span {{link.subtitle}}
+
+    #hero-logo(
+      data-tilt 
+      data-tilt-max=50
+      data-tilt-full-page-listening='true'
+      data-tilt-reverse='true'
+      data-tilt-reset='false'
+      data-tilt-max-glare='1')
+      img(src='/favicon.png')
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import 'vanilla-tilt'
 
 export default {
-  components: {
-    Logo
+  data () {
+    return {
+      links: [
+        {
+          title: 'Handsfree.js',
+          subtitle: 'Drop-in library for crafting handsfree interfaces'
+        },
+        {
+          title: 'School of AI',
+          subtitle: 'Awarded Fellowship in 2018'
+        },
+        {
+          title: 'Nonprofiters',
+          subtitle: 'Resource guide for public services and assistance programs'
+        },
+        {
+          title: 'Residency at CMU',
+          subtitle: 'Two weeks at the Studio for Creative Inquiry'
+        },
+        {
+          title: 'LinkShow',
+          subtitle: 'No-download interactive slideshow presentation platform'
+        },
+        {
+          title: 'More...',
+          subtitle: 'Everything else I\'ve been up to'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style lang="stylus">
+#hero-logo
+  width 650px
+  margin auto
+  position absolute
+  text-align: center
+  left 50%
+  margin-left -325px
+
+  img  
+    display inline-block
+    position relative
+    overflow hidden
+    animation turn 1s ease-in-out
+    margin-top 20px
+
 #hero
-  width 300px
   margin auto
   position relative
 
   ul
-    position absolute
-    margin 0
     padding 0
-    left -175px
     top 120px
     list-style none
     text-align left
     width 650px
+    margin auto
 
     li
       display inline-block
@@ -87,5 +113,19 @@ export default {
         padding 20px
 
         span
+          display block
           font-weight 400
+
+@keyframes turn
+  0%
+    top 120px
+    transform rotateY(0deg)
+    width 0
+    height 0
+
+  100%
+    top 0
+    transform rotateY(1440deg)
+    width 240px
+    height 240px
 </style>
